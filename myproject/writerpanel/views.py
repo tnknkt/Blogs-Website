c9aa617b3c9d24779d223271e2406fdb9c4dcaa6
tnknkt <tanakorn.pri@hotmail.com>
2022-03-11 10:09:57 +0700

    Update Show DataTable,Dashboard,Profile in Writer Panel

--- myproject/writerpanel/views.py
+++ myproject/writerpanel/views.py
@@ -1,6 +1,13 @@
+from tkinter import W
 from django.shortcuts import render
+from blogs.models import Blogs
+from django.db.models import Sum
 
 # Create your views here.
 
 def panel(request):
-    return render(request,"backend/index.html")
+    writer = "admin"
+    blogs = Blogs.objects.filter(writer=writer)
+    blogCount = blogs.count()
+    total = Blogs.objects.filter(writer=writer).aggregate(Sum('views')) # หาผลรวมยอดวิวบทความของนักเขียน 1 คน
+    return render(request,"backend/index.html",{"blogs":blogs,'writer':writer,'blogCount':blogCount,'total':total})
